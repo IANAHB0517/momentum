dc95b3ef2421c95dc1bd4b76bd60a51f4e1c903e
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-13 18:26:25 +0900

    object

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-const amIFat = true;
-let something;
-const wallet = null;
-console.log(something, amIFat, wallet);
+// let something;
+// const amIFat = true;
+// const wallet = null;
+// console.log(something, amIFat, wallet);
 
 
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", ];
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", ];
 
-//get Item from Array
+    //get Item from Array
 
-console.log(daysOfWeek[2]);
+// console.log(daysOfWeek[2]);
 
-// Add one more day to the array
-daysOfWeek.push("sun");
+    // Add one more day to the array
+// daysOfWeek.push("sun");
 
-console.log(daysOfWeek);
+// console.log(daysOfWeek);
 
-const toBuy = ["potato", "tomato", "pizza"];
-toBuy.push("kimbab");
+// const toBuy = ["potato", "tomato", "pizza"];
+// toBuy.push("kimbab");
 
-console.log(toBuy);
+// console.log(toBuy);
+
+// const playerName = "FTE"
+// const playerPoints = 1212;
+// const playerHandsome = true;
+// const playerFat = "Not";
+
+// const player = ["FTE", 1212, true, "Not"];
+
+const player = {
+    name: "FTE",
+    points:1212,
+    Handsome:true,
+};
+
+console.log(player);
+
+console.log(player.name);
+
+player.name = "FTE0517";
+
+console.log(player["name"]);
+
+player.lastName = "Jack";
+
+console.log(player);
