f6a88bf736edf17c67a2328c9d481fd790a7cc47
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-13 14:14:37 +0900

    constNlet

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hi");
+const a = 10;
+const b = 2;
+
+let myName = "FTE";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log ("hello " + myName)
+
+myName = "FTE0517"
+
+console.log ("Your new name is" + myName)
