5588745b74da6b03a99c80e3c1fd44cd02794641
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-13 14:35:30 +0900

    array

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var a = 10;
-const b = 2;
+const amIFat = true;
+let something;
+const wallet = null;
+console.log(something, amIFat, wallet);
 
-let myName = "FTE";
 
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log ("hello " + myName)
 
-myName = "FTE0517"
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", ];
 
-console.log ("Your new name is" + myName)
+//get Item from Array
+
+console.log(daysOfWeek[2]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+console.log(toBuy);
