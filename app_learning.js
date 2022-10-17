5e635fa8f24e1fa3cbb948703f639028c9929d5b
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-17 10:35:44 +0900

    learning_2

--- app_learning.js
+++ app_learning.js
@@ -117,45 +117,79 @@
 // calculator.crazy (12, 4);
 
 
-const calculator = {
-    plus : function(a, b) {
-        return(a + b);
-    },
-    minus : function (a, b) {
-         return(a - b);
-        },
-    divide : function (a, b) {
-         return(a / b);
-    },
-    multi : function (a, b) {
-         return(a * b);
-        },
-    crazy : function (a, b) {
-         return(a + b - a / b * a * 4 * b *-4);
-    },
-};
+// const calculator = {
+//     plus : function(a, b) {
+//         return(a + b);
+//     },
+//     minus : function (a, b) {
+//          return(a - b);
+//         },
+//     divide : function (a, b) {
+//          return(a / b);
+//     },
+//     multi : function (a, b) {
+//          return(a * b);
+//         },
+//     crazy : function (a, b) {
+//          return(a + b - a / b * a * 4 * b *-4);
+//     },
+// };
 
 
 
-const calculateResult_crazy = calculator.crazy(2,8);
-const calculateResult_minus = calculator.minus(calculateResult_crazy,8);
-const calculateResult_multi = calculator.multi(calculateResult_minus,8);
-const calculateResult_divide = calculator.divide(calculateResult_multi,4);
-const calculateResult_plus = calculator.plus(calculateResult_multi,calculateResult_divide);
+// const calculateResult_crazy = calculator.crazy(2,8);
+// const calculateResult_minus = calculator.minus(calculateResult_crazy,8);
+// const calculateResult_multi = calculator.multi(calculateResult_minus,8);
+// const calculateResult_divide = calculator.divide(calculateResult_multi,4);
+// const calculateResult_plus = calculator.plus(calculateResult_multi,calculateResult_divide);
 
-const calculateResult_final = {calculateResult_plus};
+// const calculateResult_final = {calculateResult_plus};
 
-console.log(calculateResult_final);
-
-
+// console.log(calculateResult_final);
 
 
-const age = 88;
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner + 2;
+
+
+// const age = 88;
+// function calculateKrAge(ageOfForeigner){
+//     return ageOfForeigner + 2;
+// }
+
+
+// const krAge = calculateKrAge(age);
+
+// console.log(krAge);
+
+
+// true || true === true
+// true || false == true
+// flase || true === true
+// flase || false === false
+
+// true && true === true
+// true && false == false
+// flase && true === false
+// flase && false === false
+
+const age = parseInt(
+    prompt(
+        "How old are you?"
+        )
+    );
+
+
+if(isNaN(age) || age < 0) {
+    console.log("Please write a real positive number");
+} else if (age < 18) {
+    console.log("Your are too young.");
+} else if (55 > age && age >=18 ) {
+    console.log("You can drink");
+} else if (80 > age && age >= 56) {
+    console.log("Don't drink too much.");
+} else if ( 100> age && age >= 81) {
+    console.log("You can do whatever you want if you can");
+} else if (age === 100) {
+    console.log ("Wow you are wise")
+} else if (age > 100) {
+    console.log ("glad to know your age!!")
 }
-
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
