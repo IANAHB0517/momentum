4e14a3360aa8b169073bdbdd3597fba9e16b48b1
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-18 09:48:40 +0900

    preventDefault

---
+++ app.js
@@ -0,0 +1,15 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+//     const username = loginInput.value;
+//     if(username == ""){
+      
+// }
+}
+
+
+loginForm.addEventListener("submit",onLoginSubmit );
