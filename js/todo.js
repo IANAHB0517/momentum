22b88ebf947b6b53521e776cebfe8a0af96b24c7
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-19 19:56:44 +0900

    todo_1

--- js/todo.js
+++ js/todo.js
@@ -2,21 +2,54 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintTodo(newTodo){
     const li = document.createElement("li")
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event){
     event.preventDefault();
-    // (toDoInput.value);
     const newTodo = toDoInput.value;
-    // console.log(toDoInput.value);
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintTodo(newTodo);
+    saveToDos();
 }
 
+
+
+
+
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+function sayhello(){
+    console.log("sayhello")
+}
+
+const savedTodos = JSON.parse(localStorage.getItem(TODOS_KEY));
+console.log(savedToDos);
+if(savedTodos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(sayhello);
+} 
