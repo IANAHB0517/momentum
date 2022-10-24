a97233ddfc11019e01ec276761a51d3197501c12
IANAHB0517 <105723379+IANAHB0517@users.noreply.github.com>
2022-10-24 20:32:23 +0900

    nico_done

--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,15 @@ function onGeoOk (position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log("you live in" , lat , lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    
+    fetch(url).then(response => response.json()).then(data => {
+        const city = document.querySelector("#weather span:first-child")
+        const weather = document.querySelector("#weather span:last-child")
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+    // url.normalizeurl.
 }
 
 function onGeoError (){
