1bd3228ff7ce09b865c69f408fc9fa4eef4869a3
IANAHB0517 <fromtheedge0517@gmail.com>
2022-10-25 20:36:44 +0900

    momentum_css_1

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "bcd18cafcbf9e4838a2d9efe5c5e441c";
 function onGeoOk (position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("you live in" , lat , lon);
+    // console.log("you live in" , lat , lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     
     fetch(url).then(response => response.json()).then(data => {
